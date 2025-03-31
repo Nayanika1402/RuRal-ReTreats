347969c01c1d249e869b8de607bd3fe34a875594
Rakesh De <rakeshde200@gmail.com>
2025-04-01 01:07:24 +0530

    implemented dark mode toggle feature for the home page

--- JS/script.js
+++ JS/script.js
@@ -503,3 +503,33 @@ tabButtons.forEach(button => {
         displayBlogs(button.getAttribute("data-category"));
     });
 });
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("theme-toggle");
+    const body = document.body;
+
+
+    if (localStorage.getItem("theme") === "dark") {
+        body.classList.add("dark-mode");
+    }
+
+    toggleButton.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+
+        
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    });
+});
